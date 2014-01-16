9929fb6b6b9fb90747ad9bf0f88e53c80cd9ef0d
Joel Barnes <oelarnes@gmail.com>
2014-01-15 16:00:39 -0800

    better p6

--- sum_sq_diff.py
+++ sum_sq_diff.py
@@ -1,12 +1,13 @@
 # Find the difference between the sum of the squares of the first one hundred natural 
 # numbers and the square of the sum.
 
-# Solution: The difference reduces to 2 sum(n_i * n*j) for i > j over the set in question
+# Solution: The difference reduces to 2 sum(n_i * n*j) for i > j over the set in question.
+# We can factor n_j out and sum using triangular identity.
 
 N = 100
 
-ar = [[j*k for j in range(k+1, N+1)] for k in range(1, N+1)]
+print sum([j*(N*(N+1) - j*(j+1)) for j in range(1, N+1)])
 
-s = 2*sum([sum(l) for l in ar])
+# there exist identities for these sums as well, if a closed formula is desired.
 
-print s
+# 25164150
